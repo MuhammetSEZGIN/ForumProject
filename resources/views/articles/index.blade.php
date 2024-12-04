c24640c9688df01ba577b8c18f1b7850712850f8
Muhammet Sezgin <83426745+MuhammetSEZGIN@users.noreply.github.com>
2024-12-04 13:05:08 +0300

    Update index.blade.php

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -3,12 +3,17 @@
         <x-navbar></x-navbar>
     </x-slot>
     <x-slot name="body">
-        <h1>{{$header}}</h1>
+        <h2>{{$header}}</h2>
+        <hr>
         @foreach($articles as $article)
             <h2>{{$article->title}}</h2>
-            <p>{!! \Illuminate\Support\Str::limit($article->content, 300) !!} ...
+            <div class="d-flex justify-content-end">
+                <p><small>{{$article->created_at}}</small></p>
+            </div>
+            <p>{!! \Illuminate\Support\Str::limit($article->content, 200) !!} ...
                 <a href="{{ route('showArticle', ['id' => $article->articleID]) }}">Show Article</a>
             </p>
+            <hr>
         @endforeach
         <div>
             {{ $articles->links() }}
